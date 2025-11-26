cf7df2460d2bb5a4a54a8f032f26023389445db0
shironxn <shironxn@gmail.com>
2025-11-26 18:09:03 +0700

    chore: update responsive navbar

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -20,9 +20,9 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 transistion-all duration-300 w-full ${isScrolled ? "bg-background/95 backdrop-blur-lg shadow py-3" : "bg-transparent py-6"}`}
+      className={`fixed top-0 left-0 right-0 z-50 transistion-all duration-300 w-full ${isScrolled ? "bg-background/95 backdrop-blur-lg shadow py-4" : "bg-transparent py-8"}`}
     >
-      <div className="h-full flex items-center justify-between max-w-(--breakpoint-xl) mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="container h-full flex items-center justify-between gap-8">
         <Logo />
 
         <div className="flex items-center gap-3">
