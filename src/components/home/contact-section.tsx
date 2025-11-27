400edbaa583bb9f3bbccb88d116c663a51633774
shironxn <shironxn@gmail.com>
2025-11-27 19:35:50 +0700

    chore: update link style

--- src/components/home/contact-section.tsx
+++ src/components/home/contact-section.tsx
@@ -20,21 +20,20 @@ const ContactSection = () => {
             </p>
           </div>
 
-          <div className="space-y-2 text-muted-foreground hover:cursor-pointer hover:text-foreground transition-colors duration-300">
+          <div className="space-y-2 text-muted-foreground">
             <Link
               href={`https://wa.me/${contact.phoneNumber.replaceAll(" ", "")}`}
-              className="flex gap-2 "
+              className="flex gap-2 hover:cursor-pointer hover:underline"
             >
               <PhoneIcon className="w-5 h-5" />
-              <p className="text-sm hover:cursor-pointer hover:text-foreground transition-colors duration-300">
-                {contact.phoneNumber}
-              </p>
+              <p className="text-sm">{contact.phoneNumber}</p>
             </Link>
-            <Link href={`mailto:${contact.email}`} className="flex gap-2">
+            <Link
+              href={`mailto:${contact.email}`}
+              className="flex gap-2 hover:cursor-pointer hover:underline"
+            >
               <MailIcon className="w-5 h-5" />
-              <p className="text-sm hover:cursor-pointer hover:text-foreground transition-colors duration-300">
-                {contact.email}
-              </p>
+              <p className="text-sm">{contact.email}</p>
             </Link>
           </div>
         </div>
